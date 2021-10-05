3ca3af216171e9995da7b63506a691beeed95b4a
Amanda Park <apark24@binghamton.edu>
2021-10-05 09:31:36 -0400

    Update Functions.R

--- Forecasting  - Univariate Example/Forecasting_Modeltime/ModelTimeFunctions.R
+++ Forecasting  - Univariate Example/Forecasting_Modeltime/ModelTimeFunctions.R
@@ -31,7 +31,7 @@ initialTimeSeriesEval <- function(df = dts,
   return(output)
 }
 
-modelTimeSeries <- function(train = tr,
+modeltime_forecast_series <- function(train = tr,
                             test = ts,
                             df = dts,
                             response = resp,
@@ -40,52 +40,68 @@ modelTimeSeries <- function(train = tr,
                             days = 7) {
   
   #Automate formula
-  formulaBase <- stats::as.formula(paste(response, ".", sep="~"))
+  formula_base <- stats::as.formula(paste(response, ".", sep="~"))
   
   #Recipe-free models
+  
+  #Auto-ARIMA
   arima_model <- modeltime::arima_reg() %>%
     parsnip::set_engine("auto_arima") %>%
-    parsnip::fit(formulaBase, data = train)
+    parsnip::fit(formula_base, data = train)
   
+  #Prophet
   prophet_model <- modeltime::prophet_reg() %>%
     parsnip::set_engine("prophet") %>%
-    parsnip::fit(formulaBase, data = train)
+    parsnip::fit(formula_base, data = train)
   
+  #Exponential Smoothing
   ets_model <- modeltime::exp_smoothing() %>%
     parsnip::set_engine("ets") %>%
-    parsnip::fit(formulaBase, data = train)
+    parsnip::fit(formula_base, data = train)
   
+  #TBATS
   tbats_model <- modeltime::seasonal_reg() %>%
     parsnip::set_engine("tbats") %>%
-    parsnip::fit(formulaBase, data = train)
+    parsnip::fit(formula_base, data = train)
   
+  #Neural Net
   nn_model <- modeltime::nnetar_reg() %>%
     parsnip::set_engine("nnetar") %>%
-    parsnip::fit(formulaBase, data = train)
+    parsnip::fit(formula_base, data = train)
   
+  #Seasonal Trend Loss Exp Smoothing
   stlm_ets_model <- modeltime::seasonal_reg() %>%
     parsnip::set_engine("stlm_ets") %>%
-    parsnip::fit(formulaBase, data = train)
+    parsnip::fit(formula_base, data = train)
   
+  #Seasonal Trend Loss ARIMA
   stlm_arima_model <- modeltime::seasonal_reg() %>%
     parsnip::set_engine("stlm_arima") %>%
-    parsnip::fit(formulaBase, data = train)
+    parsnip::fit(formula_base, data = train)
   
-  # forecast_table <- modeltime::modeltime_table(
-  #   arima_model,
-  #   prophet_model,
-  #   ets_model,
-  #   tbats_model,
-  #   nn_model,
-  #   stlm_ets_model,
-  #   stlm_arima_model
-  # )
+  #Naive Model
+  naive_model <- modeltime::naive_reg() %>%
+    set_engine("naive") %>%
+    fit(formula_base, data = train)
+
+  #Seasonal naive Model
+  seasonal_naive_model <- modeltime::naive_reg() %>%
+    set_engine("snaive") %>%
+    fit(formula_base, data = train)
   
-  #Models with recipes included
+  #THIEF model - Temporal Hierarchical Forecasting (Rob Hyndman)
+  thief_model <- temporal_hierarchy() %>%
+    set_engine("thief") %>%
+    fit(formula_base, train)
+
+  #ML Models - with recipes included
   rec <- recipes::recipe(Value ~ Date, train) %>%
     timetk::step_timeseries_signature(Date) %>%
+    timetk::step_fourier(Date, period = 52, K = 2) %>%
+    recipes::step_holiday(Date, holidays = timeDate::listHolidays(pattern = "US")) %>%
     recipes::step_rm(contains("am.pm"), contains("hour"), contains("minute"),
                      contains("second"), contains("xts")) %>%
+    recipes::step_nzv(all_nominal_predictors()) %>%
     recipes::step_dummy(all_nominal()) %>%
     recipes::prep()
   
@@ -98,9 +114,29 @@ modelTimeSeries <- function(train = tr,
     workflows::add_recipe(rec %>% step_rm(Date)) %>%
     parsnip::fit(train)
   
+  #MARS 
+  mars_model <- mars(mode = "regression") %>%
+    set_engine("earth") 
+  
+  mars_model_workflow <- workflows::workflow() %>%
+    workflows::add_model(mars_model) %>%
+    workflows::add_recipe(rec %>% step_rm(Date)) %>%
+    parsnip::fit(train)
+  
+  #XGBoost 
+  xg_model <- boost_tree(mode = "regression") %>%
+    set_engine("xgboost") 
+  
+  xg_model_workflow <- workflows::workflow() %>%
+    workflows::add_model(xg_model) %>%
+    workflows::add_recipe(rec %>% step_rm(Date)) %>%
+    parsnip::fit(train)
+  
   #Prophet with XGBoost Errors
-  prophet_boost_model <- modeltime::prophet_boost(seasonality_daily = "auto",
-                                       trees = 100) %>%
+  prophet_boost_model <- modeltime::prophet_boost(
+    seasonality_daily = "auto",
+    trees = 100
+    ) %>%
     parsnip::set_engine("prophet_xgboost")
 
   prophet_boost_model_workflow <- workflows::workflow() %>%
@@ -125,7 +161,14 @@ modelTimeSeries <- function(train = tr,
     nn_model,
     stlm_ets_model,
     stlm_arima_model,
-    glmnet_model_workflow
+    naive_model,
+    seasonal_naive_model,
+    thief_model,
+    glmnet_model_workflow,
+    mars_model_workflow,
+    xg_model_workflow,
+    prophet_boost_model_workflow,
+    arima_boost_model_workflow
   )
   
   acc <- forecast_table %>%
@@ -150,6 +193,25 @@ modelTimeSeries <- function(train = tr,
   fcast <- fcastVals %>%
     modeltime::plot_modeltime_forecast()
   
+  output <- list(
+    "accuracy_table" = acc,
+    "cv_plot" = crossValPlot,
+    "forecast_table" = forecast_table,
+    "forecast_plot" = fcast,
+    "forecast_values" = fcastVals
+  )
+  
+  return(output)
+  
+}
+
+ensemble_time_series <- function(train = tr,
+                                 test = ts,
+                                 df = dts,
+                                 response = resp,
+                                 predictor = pred,
+                                 folds = cvFolds,
+                                 days = 7) {
   #Model ensembling
   ensembleAvg <- forecast_table %>%
     modeltime.ensemble::ensemble_average(type = "mean")
@@ -189,20 +251,10 @@ modelTimeSeries <- function(train = tr,
   fcastEnsembleFcastPlot <- fcastEnsembleFcast %>%
     modeltime::plot_modeltime_forecast()
   
-  output <- list(
-    "accuracyTable" = acc,
-    "crossValPlot" = crossValPlot,
-    "forecastTable" = forecast_table,
-    "forecastPlot" = fcast,
-    "forecastValues" = fcastVals,
-    "accuracyEnsembleTable" = accEnsemble,
-    "ensembleCrossValPlot" = fcastEnsembleCVPlot,
-    "ensembleForecastValues" = fcastEnsembleFcast,
-    "ensembleForecastPlot" = fcastEnsembleFcastPlot
-  )
-  
-  return(output)
-  
+  output <- list("accuracyEnsembleTable" = accEnsemble,
+               "ensembleCrossValPlot" = fcastEnsembleCVPlot,
+               "ensembleForecastValues" = fcastEnsembleFcast,
+               "ensembleForecastPlot" = fcastEnsembleFcastPlot)
 }
 
 resampleTimeSeries <- function(df = dts,
